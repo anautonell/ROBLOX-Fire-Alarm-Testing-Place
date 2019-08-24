ab4bb915f31a72db0ebe9ef2b07c2d4d2f3cd37e
joshuadl12 <47647612+joshuadl12@users.noreply.github.com>
2019-08-24 16:44:40 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+  <title>Goodbye - Fire Alarm Testing Place</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
+  <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
+</head>
+<body>
+<!--JS Linking-->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
+  <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+  <script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
+  <center>
+    <h1>Fire Alarm Testing Place</h1>
+    <h1>Goodbye...</h1>
+    <p>The end of Fire Alarm Testing Place is here... A come back may soon be released, but will be under Roblox Academy, thank you for supporting Fire Alarm Testing Place.</p>
+  </center>
+  <p>Sincerely,</p>
+  <p>joshuadl12, CEO / Founder of Fire Alarm Testing Place and Roblox Academy</p>
+  <center>
+    <a class="btn btn-raised" href="https://www.robloxacademy.tk">Visit Roblox Academy</a>
+  </center>
+</body>
